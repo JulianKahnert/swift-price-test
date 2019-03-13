ed32895de8643bf18142045836a60189ed3e85e9
Julian Kahnert <mail@juliankahnert.de>
2019-03-13 22:38:20 +0100

    add wikidata endpoint

--- Sources/App/Controllers/TodoController.swift
+++ Sources/App/Controllers/TodoController.swift
@@ -74,4 +74,34 @@ final class TodoController {
         
         return ""
     }
+    
+    
+    func getWikidata(_ req: Request) throws -> String {
+        
+        let encodedQuery = "SELECT%20%3FNintendo_Entertainment_System%20%3FNintendo_Entertainment_SystemLabel%20WHERE%20%7B%0A%20%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22%5BAUTO_LANGUAGE%5D%2Cen%22.%20%7D%0A%20%20%3FNintendo_Entertainment_System%20wdt%3AP400%20wd%3AQ172742.%0A%7D%0ALIMIT%201000"
+        guard let url = URL(string: "https://query.wikidata.org/sparql?query=\(encodedQuery)&format=text/tab-separated-values") else { return "ERROR" }
+        
+        let content = try? String(contentsOf: url)
+//        print(content)
+        
+//        var request = URLRequest(url: url)
+//        request.setValue("Accept", forHTTPHeaderField: "application/json")
+//
+//        let semaphore = DispatchSemaphore(value: 0)
+//
+//        URLSession.shared.dataTask(with: request) { data, response, error in
+//
+//            print(response)
+////
+////            print("#####################################################")
+//            print(data)
+//
+//            semaphore.signal()
+//        }.resume()
+//
+//        semaphore.wait(timeout: .now() + 5)
+        
+        
+        return content ?? "ERROR"
+    }
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -16,6 +16,7 @@ public func routes(_ router: Router) throws {
     let todoController = TodoController()
     router.get("getPrice", use: todoController.getPrice)
     router.get("getPriceDelay", use: todoController.getPriceDelay)
+    router.get("getWikidata", use: todoController.getWikidata)
     router.get("todos", use: todoController.index)
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
